274de5bfe9ad8f95bd6408509aa0f449821a7b59
Rafael Marinho <rafael.mgf2001@gmail.com>
2021-05-26 15:26:25 -0300

    get method and hiding some stack tracing data

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,9 +1,13 @@
 package academy.devdojo.springboot2.controller;
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +15,22 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @AllArgsConstructor
 public class AnimeController {
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
     @GetMapping(path="list")
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Mob Psycho 100"), new Anime("Berserk"));
+        return ResponseEntity.ok(animeService.listAll());
     }
 
-    @GetMapping(path="list2")
-    public List<Anime> list2(){
-        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Mob Psycho 100"), new Anime("Berserk"));
+    @GetMapping(path="/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
+
 }
